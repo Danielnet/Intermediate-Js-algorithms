fa931c87001d74a5e725d12d2e7b04c03cd37d2b
danielnet <danielnett@hotmail.com>
2018-02-09 04:35:05 +0100

    Code challenge - Sum All Numbers In Range -

---
+++ sumAllNumbersInRange.js
@@ -0,0 +1,19 @@
+//Sum All Numbers in a Range
+//We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+function sumAll(arr) {
+  
+ var sum= 0;
+
+  return arr.reduce(function(a, b){
+   var min= Math.min(a, b);
+   var max= Math.max(a, b);
+    
+    for(var i=min+1; i<=max; i++){
+      sum+= i;
+    }
+        return sum+min;
+  });
+  
+}
+
+sumAll([5, 10]);
