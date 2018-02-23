8ab2a9d24e32d67197b468f5102f22ea720954e6
danielnet <danielnett@hotmail.com>
2018-02-23 08:14:21 +0100

    Code challenge - Boo Who, check if bool is primitive -

---
+++ booWho.js
@@ -0,0 +1,12 @@
+//Boo who
+//Check if a value is classified as a boolean primitive. Return true or false.
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(typeof(bool) === "boolean"){
+  return true;
+  }
+  else
+    return false;
+}
+
+booWho(null);
